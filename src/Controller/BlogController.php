4b5776ec4457e6f891d69219619d725e4f434fc7
Noan <noan.burelier@etu.univ-st-etienne.fr>
2025-11-14 11:52:49 +0100

    J'ai bieng avancer

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,11 +8,64 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    #[Route('/home', name: 'acceuil')]
+    public function Home(): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+        return $this->render('Home/index.html.twig', [
+            'controller_name' => 'Accueil',
+        ]);
+    }
+    
+    #[Route('/CV', name: 'mon_cv')]
+    public function CV(): Response
+    {
+        return $this->render('CV/cv.html.twig', [
+            'controller_name' => 'CV',
+        ]);
+    }
+    
+    #[Route('/Hobbies', name: 'mes_hobbies')]
+    public function Hobbies(): Response
+    {
+        return $this->render('Hobbies/hobbies.html.twig', [
+            'controller_name' => 'Hobbies',
+        ]);
+    }
+    
+    #[Route('/Contact', name: 'me_contacter')]
+    public function Contact(): Response
+    {
+        return $this->render('Contact/contact.html.twig', [
+            'controller_name' => 'Contact',
+        ]);
+    }
+    
+    #[Route('/Competences', name: 'mes_competences')]
+    public function Competences(): Response
+    {
+        return $this->render('Competences/competences.html.twig', [
+            'controller_name' => 'Competences',
+        ]);
+    }
+    #[Route('/Competences/Administrer', name: 'administrer')]
+    public function Administrer(): Response
+    {
+        return $this->render('Competences/Administrer/administrer.html.twig', [
+            'controller_name' => 'Administrer',
+        ]);
+    }
+    #[Route('/Competences/Connecter', name: 'connecter')]
+    public function Connecter(): Response
+    {
+        return $this->render('Competences/Connecter/connecter.html.twig', [
+            'controller_name' => 'Connecter',
+        ]);
+    }
+    #[Route('/Competences/Programmer', name: 'programmer')]
+    public function Programmer(): Response
+    {
+        return $this->render('Competences/Programmer/programmer.html.twig', [
+            'controller_name' => 'Programmer',
         ]);
     }
 }
